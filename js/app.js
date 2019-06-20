75c7f4f67764ab9a351d15448808840cde18df28
Dongyuan Li <dongyuanli@Dongyuans-MacBook-Pro.local>
2019-06-20 15:45:01 -0600

    commit 1 - Verbal Questions

---
+++ js/app.js
@@ -0,0 +1,52 @@
+// console.log("hw6-20");
+
+//  Verbal questions
+// What is the difference between a parameter and an argument?
+// parameter is more like a variable that applay in the function;
+//argument is more like a true or false question.
+// Within a function, what is the difference between return and console.log?
+//return don't show in the console, it only tells if it is undined or not
+//console.log, present the result on the console.
+// What are the implications of the ability of a function to return a value?
+//to make sure the vaule on console is displayed.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
