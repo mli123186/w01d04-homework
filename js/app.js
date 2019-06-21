f5cf5111908fbff2e5c44f900ff2bdfc7ea096d8
Dongyuan Li <dongyuanli@Dongyuans-MacBook-Pro.local>
2019-06-21 09:07:54 -0600

    update for #6 not done yet

--- js/app.js
+++ js/app.js
@@ -22,82 +22,82 @@
 // 	} else {
 // 		return false;
 // 	}
-// }
-//make it to array , friend helped with this problem.
+// // }
+// //make it to array , friend helped with this problem.
 
-const checkPlindrome = (string) => {
-	const word = string;
+// const checkPlindrome = (string) => {
+// 	const word = string;
 	
-	for (let i = 0; i < word.length; i ++) {
-		if (word[i].toLowerCase() == word[word.length - 1 - i].toLowerCase()) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-}
+// 	for (let i = 0; i < word.length; i ++) {
+// 		if (word[i].toLowerCase() == word[word.length - 1 - i].toLowerCase()) {
+// 			return true;
+// 		} else {
+// 			return false;
+// 		}
+// 	}
+// }
 
-console.log(checkPlindrome("Radar"));
-console.log(checkPlindrome("Borscht"));
+// console.log(checkPlindrome("Radar"));
+// console.log(checkPlindrome("Borscht"));
 
-//3. Digit Sum
-//Write a function sumDigits that accepts a number and returns the sum of its digits.
+// //3. Digit Sum
+// //Write a function sumDigits that accepts a number and returns the sum of its digits.
+
+// // const sumDigits = (number) => {
+
+// // 	let sumNumber = 0;
+// // 	while (number > 0) {
+		
+// // 		sumNumber += (number % 10);
+// // 		number = Math.floor(number/10);
+		
+
+// // 	} 
+// // 	console.log(sumNumber);
+// // }
+// // sumDigits(12);//friednd helped with this problem
+// // //make it to array!
 
 // const sumDigits = (number) => {
-
+// 	const array = number.toString().split("");
+	
 // 	let sumNumber = 0;
-// 	while (number > 0) {
-		
-// 		sumNumber += (number % 10);
-// 		number = Math.floor(number/10);
-		
-
-// 	} 
-// 	console.log(sumNumber);
-// }
-// sumDigits(12);//friednd helped with this problem
-// //make it to array!
-
-const sumDigits = (number) => {
-	const array = number.toString().split("");
 	
-	let sumNumber = 0;
-	
-	for (let i = 0; i < array.length; i++) {
-		//let sumNumber = 0;
-		sumNumber += Number(array[i]);
-	}
-	return sumNumber;
-}  
-console.log(sumDigits(12));
+// 	for (let i = 0; i < array.length; i++) {
+// 		//let sumNumber = 0;
+// 		sumNumber += Number(array[i]);
+// 	}
+// 	return sumNumber;
+// }  
+// console.log(sumDigits(12));
 
-//4. Pythagoras
+// //4. Pythagoras
  
-// Write a function calculateSide that takes two arguments: sideA and sideB, and returns 
-// the solution for sideC using the Pythagorean theorem.
+// // Write a function calculateSide that takes two arguments: sideA and sideB, and returns 
+// // the solution for sideC using the Pythagorean theorem.
 
-const calculateSide = (sideA, sideB) => {
-	let sideC;
-	sideC = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
-	return sideC;
-}
+// const calculateSide = (sideA, sideB) => {
+// 	let sideC;
+// 	sideC = Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2));
+// 	return sideC;
+// }
 
-console.log(calculateSide(8, 6));
+// console.log(calculateSide(8, 6));
 
-// // 5. Sum Array
-// //Write a function sumArray that takes an array as an argument. The array should contain numbers. 
-// //The function should return the sum of the numbers in the array.
-const sumArray = (array) => {
-	const newArray = array;
-	let sum = 0;
-	for (let i = 0; i < newArray.length; i++) {
-		sum += newArray[i];
+// // // 5. Sum Array
+// // //Write a function sumArray that takes an array as an argument. The array should contain numbers. 
+// // //The function should return the sum of the numbers in the array.
+// const sumArray = (array) => {
+// 	const newArray = array;
+// 	let sum = 0;
+// 	for (let i = 0; i < newArray.length; i++) {
+// 		sum += newArray[i];
 		
-	}
-		console.log(sum);
-}
+// 	}
+// 		return sum;
+// }
 
-sumArray([1, 2, 3, 4, 5, 6]);
+// console.log(sumArray([1, 2, 3, 4, 5, 6]));
 
 //6. Prime Numbers
 //Step One 
